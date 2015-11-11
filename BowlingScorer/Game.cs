460f4e90fdb7e2f0256d08efbb2b681c784faa20
SushanthReddyV <sushanthvenga@gmail.com>
2015-11-11 17:47:25 -0500

    Changes to be committed: 	new file:   BowlingScorer.sln 	new file:   BowlingScorer/BowlingScorer.csproj 	new file:   BowlingScorer/Game.cs 	new file:   BowlingScorer/Properties/AssemblyInfo.cs 	new file:   BowlingScorer/bin/Debug/BowlingScorer.dll 	new file:   BowlingScorer/bin/Debug/BowlingScorer.pdb 	new file:   BowlingScorer/obj/Debug/BowlingScorer.csproj.FileListAbsolute.txt 	new file:   BowlingScorer/obj/Debug/BowlingScorer.csprojResolveAssemblyReference.cache 	new file:   BowlingScorer/obj/Debug/BowlingScorer.dll 	new file:   BowlingScorer/obj/Debug/BowlingScorer.pdb 	new file:   BowlingScorer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   BowlingScorer/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   BowlingScorer/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   BowlingScorer/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   UnitTestBowlingScorer/BowlingScorerTests.cs 	new file:   UnitTestBowlingScorer/Properties/AssemblyInfo.cs 	new file:   UnitTestBowlingScorer/UnitTestBowlingScorer.csproj 	new file:   UnitTestBowlingScorer/bin/Debug/BowlingScorer.dll 	new file:   UnitTestBowlingScorer/bin/Debug/BowlingScorer.pdb 	new file:   UnitTestBowlingScorer/bin/Debug/UnitTestBowlingScorer.dll 	new file:   UnitTestBowlingScorer/bin/Debug/UnitTestBowlingScorer.pdb 	new file:   UnitTestBowlingScorer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   UnitTestBowlingScorer/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   UnitTestBowlingScorer/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   UnitTestBowlingScorer/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   UnitTestBowlingScorer/obj/Debug/UnitTestBowlingScorer.csproj.FileListAbsolute.txt 	new file:   UnitTestBowlingScorer/obj/Debug/UnitTestBowlingScorer.csprojResolveAssemblyReference.cache 	new file:   UnitTestBowlingScorer/obj/Debug/UnitTestBowlingScorer.dll 	new file:   UnitTestBowlingScorer/obj/Debug/UnitTestBowlingScorer.pdb

---
+++ BowlingScorer/Game.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScorer
+{
+    /// <summary>
+    /// Calculates the Bowling total score based on the input score(number of knocked down pins) for each roll
+    /// </summary>
+    public class Game
+    {
+        private List<int> rolls = new List<int>(21); // Total 22 integer values to allocate roll scores in a full game
+
+        private int currentRoll = 1; // Indicates current roll at a given point
+        
+        //Default Constructor
+        public Game()
+        {
+            //Instantiating the integer list(rolls) with all(21) zero values 
+            for (int i = 1; i <= 22; i++)
+            {
+                rolls.Add(0);
+            }
+        }
+
+        /// <summary>
+        /// Roll is called each time the player rolls a ball. 
+        /// </summary>
+        /// <param name="pins">The argument is the number of pins knocked down.</param>
+        public void Roll(int pins)
+        {
+            //Add the number of knocked down pins to the current roll index
+            rolls[currentRoll++] = pins;
+        }
+
+        /// <summary>
+        /// Score is called only at the very end of the game. 
+        /// </summary>
+        /// <returns>It returns the total score for that game</returns>
+        public int Score()
+        {            
+            int score = 0;  // Instantiate score with zero to startwith.
+            int rollIndex = 1; //Instantiate rollIndex with 1 indicating first roll index in frame 1
+            for (int frame = 1; frame <= 10; frame++) // looping through each frame (Total 10 frames) in a game
+            {
+                if (IsStrike(rollIndex))  // Strike Scenario 
+                {
+                    score += 10 + StrikeBonus(rollIndex);
+                    rollIndex++;  // Incrementing only once to move to next frame, as there will be no next roll in current frame for Strike Scenario
+                }
+                else if (IsSpare(rollIndex))  // Spare Scenario
+                {
+                    score += 10 + SpareBonus(rollIndex);
+                    rollIndex += 2; // Incrementing twice to move to next frame
+                }
+                else
+                {
+                    score += NormalRoll(rollIndex);  // Normal(no strike/spare) Scenario
+                    rollIndex += 2; // Incrementing twice to move to next frame
+                }
+                   
+            }
+            return score;  // Total Score
+        }
+
+        /// <summary>
+        /// Normal roll scenario, where two rolls in a frame are counted for score
+        /// </summary>
+        /// <param name="rollIndex"></param>
+        /// <returns>Frame score when no Strike or Spare</returns>
+        private int NormalRoll(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
+
+        /// <summary>
+        /// The bonus for Spare is the number of pins knocked down by the next roll
+        /// </summary>
+        /// <param name="rollIndex"></param>
+        /// <returns>Number of bonus pins</returns>
+        private int SpareBonus(int rollIndex)
+        {
+            return rolls[rollIndex + 2];
+        }
+
+        /// <summary>
+        /// The bonus for Strike is the value of the next two balls rolled
+        /// </summary>
+        /// <param name="rollIndex"></param>
+        /// <returns>Number of bonus pins</returns>
+        private int StrikeBonus(int rollIndex)
+        {
+            return rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }
+
+        /// <summary>
+        /// A spare is when the player knocks down all 10 pins in two tries. The bonus for that frame is the number of
+        /// pins knocked down by the next roll.
+        /// </summary>
+        /// <param name="rollIndex">The argument is roll number for which spare scenario need to be verified.</param>
+        /// <returns>Returns True if Spare,otherwise false</returns>
+        private bool IsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+        }
+
+        /// <summary>
+        /// A strike is when the player knocks down all 10 pins on his first try. The bonus for that frame is the value of
+        ///the next two balls rolled.
+        /// </summary>
+        /// <param name="rollIndex">The argument is roll number for which strike scenario need to be verified.</param>
+        /// <returns>Returns True if Strike,otherwise false</returns>
+        private bool IsStrike(int rollIndex)
+        {
+            return rolls[rollIndex] == 10;
+        }
+    }
+}
+
+    
+
+
---
+++ UnitTestBowlingScorer/BowlingScorerTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using BowlingScorer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestBowlingScorer
+{
+    [TestClass]
+    public class BowlingScorerTests
+    {     
+        [TestMethod]
+        public void TestRollsAsInRequirementWithInvalidTenthRollScenario()
+        {
+            var game = new Game();
+            RollPinsAsPerRequirement(game);
+            game.Roll(6); //Invalid 4th roll in 10th frame
+            Assert.AreEqual(133, game.Score());
+        }
+
+        [TestMethod]
+        public void TestRollsAsInRequirementScenario()
+        {
+            var game = new Game();
+            RollPinsAsPerRequirement(game);
+            Assert.AreEqual(133, game.Score());
+        }
+
+        [TestMethod]
+        public void TestNoPinsHitScenario()
+        {
+            var game = new Game();
+            RollPins(game, 20, 0);
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [TestMethod]
+        public void TestASpareScenario()
+        {
+            var game = new Game();
+            RolledASpare(game);
+            game.Roll(6);
+            RollPins(game, 17, 0);
+            Assert.AreEqual(22, game.Score());
+        }
+
+
+        [TestMethod]
+        public void TestAStrikeScenario()
+        {
+            var game = new Game();
+            RolledAStrike(game);
+            game.Roll(4);
+            game.Roll(4);
+            RollPins(game, 16, 0);
+            Assert.AreEqual(26, game.Score());
+        }
+
+        [TestMethod]
+        public void TestFullScoreScenario()
+        {
+            var game = new Game();
+            RollPins(game, 12, 10);
+            Assert.AreEqual(300, game.Score());
+        }
+
+       
+        
+        private void RollPinsAsPerRequirement(Game game)
+        {
+            game.Roll(1);
+            game.Roll(4);
+
+            game.Roll(4);
+            game.Roll(5);
+
+            game.Roll(6);
+            game.Roll(4);
+
+            game.Roll(5);
+            game.Roll(5);
+
+            game.Roll(10);
+
+            game.Roll(0);
+            game.Roll(1);
+
+            game.Roll(7);
+            game.Roll(3);
+
+            game.Roll(6);
+            game.Roll(4);
+
+            game.Roll(10);
+
+            game.Roll(2);
+            game.Roll(8);
+            game.Roll(6);
+        }
+
+        private void RollPins(Game game, int numberOfRolls, int pinsScored)
+        {
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                game.Roll(pinsScored);
+            }
+        }
+
+        private void RolledASpare(Game game)
+        {
+            game.Roll(8);
+            game.Roll(2);
+        }
+
+        private void RolledAStrike(Game game)
+        {
+            game.Roll(10);
+        }
+    }
+}
